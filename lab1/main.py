ec458dc429141213892f5b5844ec83e9b051c891
smela <aleksandra.smela@gmail.com>
2023-11-19 12:50:05 +0100

    some config modifications

--- lab1/main.py
+++ lab1/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import ply.lex as lex
 import scanner  # scanner.py is a file you create, (it is not an external library)
 
@@ -6,7 +7,7 @@ import scanner  # scanner.py is a file you create, (it is not an external librar
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "lab1/example.txt"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
--- lab3/main.py
+++ lab3/main.py
@@ -1,5 +1,7 @@
 
 import sys
+import os
+sys.path.append(os.getcwd()+"/..")
 import ply.yacc as yacc
 from compiling_theory.lab2 import Mparser
 from TreePrinter import TreePrinter
@@ -8,7 +10,7 @@ from TreePrinter import TreePrinter
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "lab3/example1.m"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
