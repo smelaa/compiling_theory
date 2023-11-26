a1c762d6242239a3063ac39cab6316d9b6e51971
smela <aleksandra.smela@gmail.com>
2023-11-26 16:41:05 +0100

    Mparser modifications

--- compiler/Mparser.py
+++ compiler/Mparser.py
@@ -45,22 +45,22 @@ def p_instructions(p):
     else:
         p[0] = p[1] + [p[2]]
 
-def p_program_ins(p):
-    """program_ins : PRINT print_value ';'
-                | assign ';'
-                | cond_instruction
-                | while_instruction
-                | for_instruction
-                | '{' program '}'
-                | BREAK ';'
-                | CONTINUE ';'
-                | RETURN operation ';'
-                | COMMENT""" # jak dla mnie to to tu musi być, bo inaczej są błędy, na razie to tu zostawmy
-    if p[1] == "break" or p[1] == "continue" : p[0] = AST.StatementList(AST.KeyWords(p[1]))
-    elif len(p)==2 or len(p) == 3: p[0]=p[1]
-    elif p[1] == "print": p[0] = AST.Print(p[2])
-    elif p[1] == "return": p[0] = AST.Return(p[2])
-    else: p[0] = p[2]
+def p_return(p):
+    """program_ins : RETURN operation ';'"""
+    p[0] = AST.Return(p[2])
+
+def p_keywords(p):
+    """program_ins : BREAK ';'
+                | CONTINUE ';'"""
+    p[0]=AST.KeyWords(p[1])
+
+def p_bracket_ins(p):
+    """program_ins : '{' program '}'"""
+    p[0] = p[2]
+
+def p_print(p):
+    """program_ins : PRINT print_value ';'"""
+    p[0] = AST.Print(p[2])
 
 def p_print_value(p):
     """print_value : operation ',' print_value
@@ -69,9 +69,9 @@ def p_print_value(p):
     else: p[0] = AST.Values(p[1])
 
 def p_assign(p):
-    """assign : id assign_operator operation
-                | id '[' index ']' assign_operator operation"""
-    if len(p) == 4 : p[0] = AST.Assign(p[2], p[1], p[3])
+    """program_ins : id assign_operator operation ';'
+                | id '[' index ']' assign_operator operation ';'"""
+    if len(p) == 5 : p[0] = AST.Assign(p[2], p[1], p[3])
     else: p[0] = AST.RefAssign(p[5], p[1], p[3], p[6])
 
 def p_assign_operator(p):
@@ -89,7 +89,7 @@ def p_index(p):
     else: p[0] = AST.Index(AST.IntNum(p[1]))
 
 def p_vector(p):
-    """vector : '[' vector_val ']'"""
+    """operation : '[' vector_val ']'"""
     p[0] = AST.Vector(p[2])
 
 def p_vector_val(p):
@@ -98,29 +98,41 @@ def p_vector_val(p):
     if len(p) == 4 : p[0] = AST.Values(p[1], p[3])
     else: p[0] = AST.Values(p[1])
 
-def p_operation(p):
+def p_binary_operation(p):
     """operation : operation add_operator operation %prec ADD
-                | operation mul_operator operation %prec MUL
-                | '-' operation %prec UMINUS
-                | operation "'" 
-                | '(' operation ')'
-                | id
-                | INTNUM
-                | FLOATNUM
-                | fid '(' operation ')'
-                | vector
-                | string"""
-    if len(p) == 5: p[0] = AST.Fid(p[1], p[3])
-    elif len(p) == 4 and p[1] != '(' : p[0] = AST.BinExpr(p[2], p[1], p[3])
-    elif len(p) == 4 : p[0] = p[2]
-    elif len(p) == 3 and p[1] == '-': p[0] = AST.UnaryExpr(p[1], p[2])
-    elif len(p) == 3: p[0] = AST.UnaryExpr(p[2], p[1])
-    elif isinstance(p[1], int): p[0] = AST.IntNum(p[1])
-    elif isinstance(p[1], float): p[0] = AST.FloatNum(p[1])
-    else: p[0] = p[1]
+                | operation mul_operator operation %prec MUL"""
+    p[0] = AST.BinExpr(p[2], p[1], p[3])
+
+def p_bracket_operation(p):
+    """operation : '(' operation ')' """
+    p[0] = p[2]
+
+def p_negative(p):
+    """operation : '-' operation %prec UMINUS """
+    p[0] = AST.UnaryExpr(p[2], p[1])
+
+def p_transpose(p):
+    """operation : operation "'" """
+    p[0] = AST.UnaryExpr(p[2], p[1])
+
+def p_special_function(p):
+    """operation : fid '(' operation ')'"""
+    p[0] = AST.Fid(p[1], p[3])
+
+def p_int(p):
+    """operation : INTNUM"""
+    p[0] = AST.IntNum(p[1])
+
+def p_float(p):
+    """operation : FLOATNUM"""
+    p[0] = AST.FloatNum(p[1])
+
+def p_variable(p):
+    """operation : id"""
+    p[0] = p[1]
 
 def p_string(p):
-    """string : STRING"""
+    """operation : STRING"""
     p[0] = AST.String(p[1])
 
 def p_mul_operator(p):
@@ -148,7 +160,7 @@ def p_fid(p):
     p[0] = p[1]
 
 def p_cond_instruction(p):
-    """cond_instruction : IF '(' condition ')' program_ins %prec IFX
+    """program_ins : IF '(' condition ')' program_ins %prec IFX
                         | IF '(' condition ')' program_ins ELSE program_ins"""
     if len(p) == 6: p[0] = AST.If(p[3], p[5])
     else: p[0] = AST.If(p[3], p[5], p[7])
@@ -167,11 +179,11 @@ def p_comparison_operator(p):
     p[0] = p[1]
 
 def p_while_instruction(p):
-    """while_instruction : WHILE '(' condition ')' program_ins"""
+    """program_ins : WHILE '(' condition ')' program_ins"""
     p[0] = AST.While(p[3], p[5])
 
 def p_for_instruction(p):
-    """for_instruction : FOR id '=' range program_ins"""
+    """program_ins : FOR id '=' range program_ins"""
     p[0] = AST.For(p[1], p[2], p[4], p[5])
 
 def p_range(p):
--- lab3/main.py
+++ lab3/main.py
@@ -9,7 +9,7 @@ from compiling_theory.compiler import Mparser
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "lab3/example3.m"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "lab3/example1.m"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
