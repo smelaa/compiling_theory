93660516c4a7bfacc0f0769de98419166ce73d4d
smela <aleksandra.smela@gmail.com>
2023-12-04 12:12:25 +0100

    lab5 init commit

---
+++ lab5/main.py
@@ -0,0 +1,37 @@
+
+
+import sys
+import os
+sys.path.append(os.getcwd()+"/..")
+import ply.yacc as yacc
+from compiling_theory.compiler.Mparser import Mparser
+from compiling_theory.compiler.AST import TreePrinter
+from compiling_theory.compiler.TypeChecker import TypeChecker
+from Interpreter import Interpreter
+
+
+if __name__ == '__main__':
+
+    try:
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
+        file = open(filename, "r")
+    except IOError:
+        print("Cannot open {0} file".format(filename))
+        sys.exit(0)
+
+    Mparser = Mparser()
+    parser = yacc.yacc(module=Mparser)
+    text = file.read()
+
+    ast = parser.parse(text, lexer=Mparser.scanner)
+
+    # Below code shows how to use visitor
+    typeChecker = TypeChecker()   
+    typeChecker.visit(ast)   # or alternatively ast.accept(typeChecker)
+
+    ast.accept(Interpreter())
+    # in future
+    # ast.accept(OptimizationPass1())
+    # ast.accept(OptimizationPass2())
+    # ast.accept(CodeGenerator())
+    
