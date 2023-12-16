4f03e5fcbb6bad556921ea83c41ac986a0ce9fde
Aleksandra Smela <aleksandra.smela@gmail.com>
2023-12-16 11:11:54 +0100

    exit if there is semantic exception

--- lab5/main.py
+++ lab5/main.py
@@ -11,7 +11,7 @@ from Interpreter import Interpreter
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "lab5/examples/fibonacci.m.txt"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "lab5/examples/triangle.m.txt"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
